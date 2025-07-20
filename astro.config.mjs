538b15aef42f355e9cb711a2f396ada3dc0d737f
Jonathan Olbes <olbes.es@gmail.com>
2025-07-20 22:36:56 +0200

    Estructurando proyecto y creando modulos a utilizar como base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,23 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sixcores.com',
+  base: '/',
+
+  // URLs terminan con "/" → mejor para SEO
+  trailingSlash: 'always',
+
+  // Genera archivos estáticos para despliegue en Vercel
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react()]
 });
